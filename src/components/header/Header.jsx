c5ceca0323a189533081b97e1244cf4ad2cb6bc8
Bhushan Gawande <brgawande@gmail.com>
2024-08-01 08:25:27 +0530

    react redux middleware question

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import {
   Disclosure,
   DisclosureButton,
@@ -21,12 +22,19 @@ function classNames(...classes) {
 }
 
 export default function Header() {
+  const location = useLocation();
+
+  const updatedNavigation = navigation.map((item) => ({
+    ...item,
+    current: location.pathname === item.href,
+  }));
+
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
+            {/* Mobile menu button */}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
@@ -50,7 +58,7 @@ export default function Header() {
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {navigation.map((item) => (
+                {updatedNavigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
@@ -127,7 +135,7 @@ export default function Header() {
 
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
+          {updatedNavigation.map((item) => (
             <DisclosureButton
               key={item.name}
               as="a"
--- src/components/react/ReactInterviewQuestions.jsx
+++ src/components/react/ReactInterviewQuestions.jsx
@@ -1,7 +1,120 @@
 import React from "react";
+import reduxmiddlewareimg1 from "../../assets/reduxmiddlewareimg1.png";
+import reduxmiddlewareimg2 from "../../assets/reduxmiddlewaresimg2.png";
+
+const questions = [
+  {
+    id: 1,
+    question: "What are Middlewares? Types of Middlewares used in React",
+    answer: [
+      {
+        id: 1,
+        ans: "Middlewares are something which gives a bridge between processsing incoming  request and outgoing response between the client and server",
+      },
+      {
+        id: 2,
+        ans: "Middleware in React (particularly with Redux) helps in handeling side effect, making asynchronous action and maintaning cleaner and more manageable code.",
+      },
+      {
+        id: 3,
+        ans: "Common Middleware used in React Application with Redux are:- <br /> 1. Redux Thunk <br /> 2. Redux Saga <br /> 3. Redux Logger <br /> 4. Redux Promise ",
+      },
+      {
+        id: 3,
+        img: reduxmiddlewareimg1,
+        ans: "Hum apne React Application se jab bhi koi action dispatch karte hai to action directly reducer tak pohoch jata hai aur reducer pe jake usko update kar deta hai.",
+      },
+      {
+        id: 4,
+        ans: "But agar hume koi API call karni ho to hum reducer ke aandar nhi kar sakte hai, because reducer pure function hote hai...pure function means kabhi bhi koi input uske aandar dala jaye to output same ho. <br /> (same input = same output) === pure function",
+      },
+      {
+        id: 5,
+        ans: "Pure function predictable hone chahiye, Lekin API jab call hoti hai wo predictable nhi hoti hai, kabhi network issue aa jata hai, ya kayi baar wo fail ho jati hai",
+      },
+      {
+        id: 6,
+        ans: "To iske liye hum reducer tak pohochne se pehele hi ek naya function add kar dete hai, jo ki hota hai, use hum middleware kehte hai.",
+      },
+      {
+        id: 7,
+        img: reduxmiddlewareimg2,
+        ans: "Basically middleware ek function hota hai ki dispatch karne ke aur reducer tak pohochne ke bich me wo chalta hai.",
+      },
+      {
+        id: 8,
+        ans: "to humara jo action hai wo pehele middleware tak pohochenga aur agar middleware chahenga to tabhi wo reducer tak pohochenga wo bich me usko rok bhi sakta hai agar koi error aaye to.",
+      },
+      {
+        id: 9,
+        ans: "Middleware concept can also apply to other areas in react application such as:- <br /> 1. Middleware for routing - Custom route handling logic in react router such as authentication guards. <br /> 2. Middlewares for API calls - function that intercepts and modify request or response like axios interceptors.",
+      },
+    ],
+  },
+];
+
+const highlightWords = (text, words) => {
+  let highlightedText = text;
+  words.forEach((word) => {
+    const regex = new RegExp(`(${word})`, "gi");
+    highlightedText = highlightedText.replace(regex, "<mark>$1</mark>");
+  });
+  return highlightedText;
+};
 
 const ReactInterviewQuestions = () => {
-  return <div>ReactInterviewQuestions</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div className="bg-lightblue py-24  sm:py-10">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 bg-lightblue">
+              {questions && questions.length > 0 ? (
+                <>
+                  {questions.map((item, index) => (
+                    <div
+                      key={index}
+                      className="mx-auto rounded-xl bg-white max-w-full py-6 shadow-md p-10 my-10 border-2 border-[rgb(244,244,244)] lg:text-center"
+                    >
+                      <p className="capitalize mt-2 pb-4 text-start text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        {item.question}
+                        {" ?"}
+                      </p>
+                      {item?.answer && item?.answer.length > 0 ? (
+                        <>
+                          {item.answer.map((i) => (
+                            <div key={i.id} className="relative">
+                              {i?.img && (
+                                <div>
+                                  <img src={i?.img} alt="" />
+                                </div>
+                              )}
+                              <p className="absolute top-[3%] py-2 text-xl font-bold">
+                                {i.id}.
+                              </p>
+                              <p
+                                className="capitalize text-start pl-6 py-2 text-xl font-semibold leading-8 text-gray-800"
+                                dangerouslySetInnerHTML={{
+                                  __html: highlightWords(i.ans, [
+                                    "marking",
+                                    "meaning",
+                                  ]),
+                                }}
+                              ></p>
+                            </div>
+                          ))}
+                        </>
+                      ) : null}
+                    </div>
+                  ))}
+                </>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReactInterviewQuestions;
